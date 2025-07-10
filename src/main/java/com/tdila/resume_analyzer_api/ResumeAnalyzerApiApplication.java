2618125f9ace082d4cb12a8033d11b15c7eef48f
Tharusha Dilhara <119704521+TDila@users.noreply.github.com>
2025-07-10 17:39:45 +0530

    Initial commit

---
+++ src/main/java/com/tdila/resume_analyzer_api/ResumeAnalyzerApiApplication.java
@@ -0,0 +1,13 @@
+package com.tdila.resume_analyzer_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResumeAnalyzerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResumeAnalyzerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tdila/resume_analyzer_api/ResumeAnalyzerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdila.resume_analyzer_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResumeAnalyzerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
